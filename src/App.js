14f567927321e23ae8f0d582fb49178768fe7222
HexTheCode <alopezgarcia380@gmail.com>
2020-08-26 21:27:35 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 100%; /*background-size: 300px;*/
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  cursor: pointer;
+`;
 
 function App() {
+  const apiRequest = () => {
+    console.log("Req");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button onClick={apiRequest}>Obtener Frase</Button>
+    </Container>
   );
 }
 
